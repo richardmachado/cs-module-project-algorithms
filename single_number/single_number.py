e3232f71f8375bb3ccde742957410cbd43889822
Richard Machado <mmachado.richard@gmail.com>
2020-08-20 12:11:50 -0700

    starting day 2

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,12 +3,21 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    #iterate through the numbers
-    #find the nonduplicate
     
-    pass
+    # create a set
+    s = set()
+    
+    # loop through the numbers
+    for i in arr:
+        #if this is in the set already, remove it
+        if i in s :
+            s.remove(i)
+        #otherwise, it's a single, so add it
+        else:
+            s.add(i)
+    #find the nonduplicate
+
+    return list(s)[0]
 
 
 if __name__ == '__main__':
